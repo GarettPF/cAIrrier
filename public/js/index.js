44297080dd76f871b9d1bfd50b2e222f509791c1
Garett Pascual-Folster <garettpascual-folster@Garetts-MacBook-Air.local>
2019-07-27 22:21:14 -1000

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,26 @@
+const G_TRACKING_ID = 'UA-143960290-1';
+const HOTJAR_ID = 1419537;
+(function($){
+  $(function(){
 
-
-//initializing parallax code
-$(document).ready(function(){
+    $('.sidenav').sidenav();
     $('.parallax').parallax();
-  });
+
+	  <!-- Global site tag (gtag.js) - Google Analytics -->
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', G_TRACKING_ID);
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
 
 
-//mixpanel for tracking
-  //mixpanel.track("Video play");
+(function(h,o,t,j,a,r){
+	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+	h._hjSettings={hjid:HOTJAR_ID,hjsv:6};
+	a=o.getElementsByTagName('head')[0];
+	r=o.createElement('script');r.async=1;
+	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+	a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
