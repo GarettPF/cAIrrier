ef7d8c0dbd64678c9e0e1daaa42f383eb7916f20
Garett Pascual-Folster <garettpascual-folster@rrcs-66-27-37-202.west.biz.rr.com>
2019-07-28 14:18:35 -1000

    Update

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-    $('select').material_select();
-  });
+// $(document).ready(function() {
+//     $('select').material_select();
+//   });
 
 
+var form = document.getElementById("form");
+var confirm = document.getElementById("confirm");
+
+var submit = document.getElementById("submit");
+
+submit.addEventListener('click', ()=> {
+    form.style.display = "none";
+    confirm.style.display = "block";
+})
