44145db46be8fd91568d151f9d6c10536d23fb77
caslabs <jeraldydcascayan@gmail.com>
2019-08-01 00:14:44 -1000

    update

--- public/js/flightChat.js
+++ public/js/flightChat.js
@@ -8,20 +8,30 @@ let msgContainer = document.querySelector('#chatContainer');
 //var request = require('request');
 
 
-function updateScroll(){
+function updateScroll() {
     chat_element.scrollTop = chat_element.scrollHeight;
 }
 
 function generateTxtMSG(value) {
-    return  `                                
+    return `                                
     <div class="container darker">
     <img src="../../images/test2.png" alt="Avatar" class="right">
     <p>${value}</p>
     <span class="time-left">11:05</span>
     </div>`
-    
+
 }
 
+
+function generateOtherTxtMsg(value) {
+    return `<div class="container">
+    <img src="../../images/test.png" alt="Avatar">
+    <p>${value}</p>
+    <span class="time-right">11:00</span>
+</div>
+`
+
+}
 
 
 
@@ -38,22 +48,32 @@ function generateTxtMSG(value) {
 
 
 var apiLink = 'https://api.twilio.com/2010-04-01/Accounts/AC49e102719065ef4f9434960a7ab24ddb/Messages.json'
+
 function waitForPing() {
 
     console.log('test');
     //auto calls JSON every 5 seconds. or everytime you click? ma
-        setTimeout(function(){ 
-            
-            
-            request({
-                url: apiLink,
-                json: true
-              }, function(error, response, body) {
-                console.log(body);
-              });
-            
-            ; }, 1000);
-        //millisends
+    setTimeout(function () {
+
+
+        request({
+            url: apiLink,
+            method: 'GET',
+            auth: {
+                'user': 'AC49e102719065ef4f9434960a7ab24ddb',
+                'pass': 'f3becea26411a3052e3f9394fb8b5428'
+            },
+            json: true
+        }, function (error, response, body) {
+            //generateOtherTxtMsg(body.messages[0])
+            console.log(body.messages[1].body)
+
+            msgContainer.innerHTML += generateOtherTxtMsg(body.messages[1].body);
+        });
+
+        ;
+    }, 10000);
+    //millisends
 }
 
 
@@ -85,20 +105,15 @@ let chat_container = document.querySelector('#chatContainer');
 
 
 
-buttonSend.addEventListener('click', ()=>  {
-<<<<<<< HEAD
+buttonSend.addEventListener('click', () => {
     console.log('test');
-    msgContainer.innerHTML+= generateTxtMSG(msgValue.value);
-    //request(generateAPI(msgValue.value));
-    waitForPing()
-=======
-    console.log();
     msgContainer.innerHTML += generateTxtMSG(msgValue.value);
+    //request(generateAPI(msgValue.value));
+    //waitForPing()
     request(generateAPI(msgValue.value));
 
 
     msgValue.value = '';
->>>>>>> a4a991bc3d8b4e29428b92e2c666e9b61bec2434
-    updateScroll();
+    waitForPing();
 
 })
