7302ce335533ee32107a172aa17034079f2f7c07
caslabs <jeraldydcascayan@gmail.com>
2019-07-31 14:51:23 -1000

    test flight

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,32 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
-// DANGER! This is insecure. See http://twil.io/secure
-const accountSid = 'AC49e102719065ef4f9434960a7ab24ddb';
-const authToken = 'f3becea26411a3052e3f9394fb8b5428';
-const client = require('twilio')(accountSid, authToken);
-console.log("test");
+'use strict';
+var request = require('request');
+let btn = document.querySelector('#btnSend');
+let txtVal = document.querySelector('#txtValue');
 
-//this is a test in node.js instead of python.
-client.messages
-      .create({body: 'Hi there!', from: '+18084259665', to: '+18084857608'})
-      .then(message => console.log(message.sid));
 
+function generateAPI(msg) {
+    return {
+        url: 'https://api.twilio.com/2010-04-01/Accounts/AC49e102719065ef4f9434960a7ab24ddb/Messages.json',
+        method: 'POST',
+        auth: {
+            'user': 'AC49e102719065ef4f9434960a7ab24ddb',
+            'pass': 'f3becea26411a3052e3f9394fb8b5428'
+        },
+        form: {
+            To: '+18084857608',
+            From: '+18084259665',
+            Body: msg
+        }
+    }
+}
+
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+btn.addEventListener("click", () => {
+    request(generateAPI(txtVal.value), callback);
+        })
--- public/js/flightChat.js
+++ public/js/flightChat.js
@@ -1,40 +1,46 @@
 let chat_element = document.querySelector('#chatApplication');
 let buttonSend = document.querySelector('#buttonSend');
 let msgValue = document.querySelector('#msgValue');
-
+var request = require('request');
 let msgContainer = document.querySelector('#chatContainer');
 
 
-const accountSid = 'AC49e102719065ef4f9434960a7ab24ddb';
-const authToken = 'f3becea26411a3052e3f9394fb8b5428';
-const client = require('twilio')(accountSid, authToken);
-
-
-
+//var request = require('request');
 
 
 function generateTxtMSG(value) {
     return  `                                
     <div class="container darker">
-    <img src="/w3images/avatar_g2.jpg" alt="Avatar" class="right">
+    <img src="../../images/test2.png" alt="Avatar" class="right">
     <p>${value}</p>
     <span class="time-left">11:05</span>
     </div>`
     
 }
+function generateAPI(msg) {
+    return {
+        url: 'https://api.twilio.com/2010-04-01/Accounts/AC49e102719065ef4f9434960a7ab24ddb/Messages.json',
+        method: 'POST',
+        auth: {
+            'user': 'AC49e102719065ef4f9434960a7ab24ddb',
+            'pass': 'f3becea26411a3052e3f9394fb8b5428'
+        },
+        form: {
+            To: '+18084857608',
+            From: '+18084259665',
+            Body: msg
+        }
+    }
+}
 
 let chat_container = document.querySelector('#chatContainer');
 
-console.log('dank memes');
 
 
 
 buttonSend.addEventListener('click', ()=>  {
-    console.log(msgValue.value);
-    console.log(chat_container);
+    console.log();
     msgContainer.innerHTML+= generateTxtMSG(msgValue.value);
-    client.messages
-      .create({body: msgValue.value, from: '+18084259665', to: '+18084857608'})
-      .then(message => console.log(message.sid));
+    request(generateAPI(msgValue.value));
 
 })
